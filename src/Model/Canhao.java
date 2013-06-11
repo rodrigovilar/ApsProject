3158ee21c0b3d9567476e92786f043a22662336f
jonnathann <jonnathann@dce.ufpb.br>
2013-06-11 20:51:22 -0300

    Criando os atributos da classe Canhao

---
+++ src/Model/Canhao.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Canhao {
+	
+	private Tiro tiro; //Objeto tiro
+	private int posicaoX = 250;//Posição X do canhao em relação a tela
+	private int posicaoY = 500;//Posição Y do canhao em relação a tela
+
+}
