5882502f1262b392dd4d5550a804c387715e590e
jonnathann <jonnathann@dce.ufpb.br>
2013-06-11 20:54:46 -0300

    Criando método getPosicaoX

--- src/Model/Canhao.java
+++ src/Model/Canhao.java
@@ -2,8 +2,13 @@ package Model;
 
 public class Canhao {
 	
-	private Tiro tiro; //Objeto tiro
+	//Atributos 
+	private Tiro tiro; //Referencia do tipo Tiro(referente a classe Tiro)
 	private int posicaoX = 250;//Posição X do canhao em relação a tela
 	private int posicaoY = 500;//Posição Y do canhao em relação a tela
+	
+	public int getPosicaoX() {//Método que recupera atributo privado posicaoX
+		return posicaoX;
+	}
 
 }
