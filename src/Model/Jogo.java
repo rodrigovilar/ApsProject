fecaad2250f7cc4ae6347461771a1388cbbafc95
jonnathann <jonnathann@dce.ufpb.br>
2013-06-11 21:46:03 -0300

    Criando método isVerificarJogadorNaFase

--- src/Model/Jogo.java
+++ src/Model/Jogo.java
@@ -77,6 +77,9 @@ public class Jogo {
 		gerenciadorFase.inserirJogadorNaFaseDisponivel(jogador);
 	}
 	
+	public boolean isVerificarJogadorNaFase(Jogador jogador){
+		return gerenciadorFase.isVerificarJogadorNaFaseDisponivel(jogador);
+	}
 
 
 
