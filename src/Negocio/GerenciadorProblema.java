a681782378bf95f101dabbf3001cf0022a9b84ef
jonasmendonca <jonas.mendonca@dce.ufpb.br>
2013-06-11 22:19:44 -0300

    Criação dos Atributos do GerenciadorProblema

--- src/Negocio/GerenciadorProblema.java
+++ src/Negocio/GerenciadorProblema.java
@@ -1,5 +1,9 @@
 package Negocio;
 
 public class GerenciadorProblema {
+	private ArrayList<Problema> problemas = new ArrayList<Problema>();
+	private Professor professor;
+	ArrayList<Professor> professoresCadastrados;
+	private ArrayList<Balao> baloes= new ArrayList<Balao>();
 
 }
