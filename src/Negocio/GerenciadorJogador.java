5e68d12b16a72d0d7590b1d7b3ae8dda4a7fe604
Josue da Silva Gomes Junior <josue.gomes@dce.ufpb.br>
2013-06-11 21:49:37 -0300

    criado metodo cadastrar jogador

--- src/Negocio/GerenciadorJogador.java
+++ src/Negocio/GerenciadorJogador.java
@@ -4,4 +4,10 @@ import java.util.ArrayList;
 public class GerenciadorJogador {
 	private ArrayList<Jogador> jogadores = new ArrayList<Jogador>();
 
+	public void cadastrarJogador(Jogador jogador) throws ObjetoJaExistenteException {
+		if(buscarJogador(jogador)){
+			throw new ObjetoJaExistenteException("Esse jogador já existe");
+		}
+		jogadores.add(jogador);
+	}
 }
