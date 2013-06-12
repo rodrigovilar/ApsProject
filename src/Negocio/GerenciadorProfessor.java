d1bd8f01271d645213339c364a45783b6d61abbb
Josue da Silva Gomes Junior <josue.gomes@dce.ufpb.br>
2013-06-11 22:21:23 -0300

    criado metodo cadastrarProfessor

--- src/Negocio/GerenciadorProfessor.java
+++ src/Negocio/GerenciadorProfessor.java
@@ -4,5 +4,13 @@ import java.util.ArrayList;
 public class GerenciadorProfessor {
 	private ArrayList<Professor> professores = new ArrayList<Professor>();
 	
-	
+	public void cadastrarProfessor(Professor professor) throws ObjetoJaExistenteException, ObjetoInexistenteException {
+		if(buscarProfessor(professor)){
+			throw new ObjetoJaExistenteException("Esse professor já existe");
+		}
+		if(professor.getSenha() == null || professor.getSenha().length() < 4){
+			throw new ObjetoInexistenteException("Senha inválida");
+		}
+		professores.add(professor);
+	}
 }
