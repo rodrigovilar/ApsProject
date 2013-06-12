bc2e456600b17be7736df0c56a97d4ad1729f78c
jonasmendonca <jonas.mendonca@dce.ufpb.br>
2013-06-11 22:11:39 -0300

    Criação do Atributo Resposta e do método getReposta

--- src/Model/Balao.java
+++ src/Model/Balao.java
@@ -2,20 +2,10 @@ package Model;
 
 public class Balao {
 
-	private int conteudo;
-	private int id;
-	
-	public int getConteudo() {
-		return conteudo;
-	}
-	public void setConteudo(int conteudo) {
-		this.conteudo = conteudo;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id){
-		this.id = id;
+	private int resposta;
+
+	public int getResposta() {
+		return resposta;
 	}
 
 }
