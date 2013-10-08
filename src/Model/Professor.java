9d0da838120e74ac631fe947f9a602f569ceb264
Josue da Silva Gomes Junior <josue.gomes@dce.ufpb.br>
2013-10-07 22:28:09 -0300

    add metodos

--- src/Model/Professor.java
+++ src/Model/Professor.java
@@ -4,7 +4,20 @@ import java.io.Serializable;
 
 //@SuppressWarnings("serial")
 public class Professor extends Usuario  implements Serializable { //
-	
+	/*private String nome;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	private int ID;*/
 	public Professor(){
 		int id = super.getId();
 		id++;
