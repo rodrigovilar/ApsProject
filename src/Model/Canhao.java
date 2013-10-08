d6ef0e792d3cfc14679c9c89444ab62fb8f76d59
Josue da Silva Gomes Junior <josue.gomes@dce.ufpb.br>
2013-10-07 22:14:43 -0300

    iniciando variaveis

--- src/Model/Canhao.java
+++ src/Model/Canhao.java
@@ -2,8 +2,14 @@ package Model;
 
 public class Canhao {
 	
+	//Atributos 
+	private Municao municao; //Referencia do tipo Tiro(referente a classe Tiro)
+	private int posicaoX = 250;//Posi�‹o X do canhao em rela�‹o a tela
+	private int posicaoY = 500;//Posi�‹o Y do canhao em rela�‹o a tela
 	
-	
+	public int getPosicaoX() {//MŽtodo que recupera atributo privado posicaoX
+		return posicaoX;
+	}
 	
 	public void setPosicaoX(int posicaoX) {//MŽtodo que muda o valor do atributo posicaoX
 		this.posicaoX = posicaoX;
