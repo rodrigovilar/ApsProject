9f65f73099b07242d29e033d3d08c7c3213a5783
jonnathann <>
2013-10-07 22:52:57 -0300

    comitando

--- src/Negocio/GerenciadorProfessor.java
+++ src/Negocio/GerenciadorProfessor.java
@@ -20,5 +20,38 @@ public class GerenciadorProfessor {
 		return gerenciadorProblema;
 	}
 	
+	public void cadastrarProfessor(Professor professor) throws ObjetoJaExistenteException, ObjetoInexistenteException, IOException {
+		if(buscarProfessor(professor)){
+			throw new ObjetoJaExistenteException("Esse professor já existe");
+		}
+		if(professor.getSenha() == null || professor.getSenha().length() < 4){
+			throw new ObjetoInexistenteException("Senha invï¿½lida");
+		}
+		professores.add(professor);
+		//Persistencia.escreverNoArquivo(professor);
+	}
 	
+	public void removerProfessor(Professor professor) throws ObjetoInexistenteException{
+		if(!buscarProfessor(professor)){
+			throw new ObjetoInexistenteException("Esse professor nï¿½o existe");
+		}
+		professores.remove(professor);
+	}
+	
+	private boolean buscarProfessor(Professor professor) {
+		for(Professor p:professores){
+			if(p.getNome().equals(professor.getNome())){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int getQuantidadeDeProfessoresCadastrados(){
+		return professores.size();
+	}
+	
+	public ArrayList<Professor> listarProfessores(){
+		return professores;
+	}
 }
