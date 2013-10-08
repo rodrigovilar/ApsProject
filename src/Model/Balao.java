fcd88d1db4ccdf77be3bae7f84ced98902e52738
Josue da Silva Gomes Junior <josue.gomes@dce.ufpb.br>
2013-10-07 22:06:51 -0300

    criação da classe balao

--- src/Model/Balao.java
+++ src/Model/Balao.java
@@ -1,3 +1,13 @@
 package Model;
 
+public class Balao {
 
+	private int resposta;
+
+	public int getResposta() {
+		return resposta;
+	}
+	public void setResposta(int resposta) {
+		this.resposta = resposta;
+	}
+}
