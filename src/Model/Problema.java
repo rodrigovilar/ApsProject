9d4e99e84a2fded40e1028af9b896403d1d5d897
Josue da Silva Gomes Junior <josue.gomes@dce.ufpb.br>
2013-10-07 22:27:05 -0300

    add geters and seters

--- src/Model/Problema.java
+++ src/Model/Problema.java
@@ -6,5 +6,26 @@ public class Problema {
 	private String questao;
 	private int resposta;
 	
+	public int getId() {
+		return id;
+	}
 	
+	public Problema() {
+		id++;
+	}
+	
+	public String getQuestao() {
+		return questao;
+	}
+	
+	public void setQuestao(String questao) {
+		this.questao = questao;
+	}
+	
+	public int getResposta() {
+		return resposta;
+	}
+	public void setResposta(int resposta) {
+		this.resposta = resposta;
+	}
 }
