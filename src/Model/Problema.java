9c790411f4b69d93ab0027a1bd0152350d582430
Josue da Silva Gomes Junior <josue.gomes@dce.ufpb.br>
2013-10-07 22:26:42 -0300

    refatorando

--- src/Model/Problema.java
+++ src/Model/Problema.java
@@ -2,27 +2,9 @@ package Model;
 
 public class Problema {
 	
+	private int id;
+	private String questao;
+	private int resposta;
 	
-	public int getId() {
-		return id;
-	}
 	
-	public Problema() {
-		id++;
-	}
-	
-	public String getQuestao() {
-		return questao;
-	}
-	
-	public void setQuestao(String questao) {
-		this.questao = questao;
-	}
-	
-	public int getResposta() {
-		return resposta;
-	}
-	public void setResposta(int resposta) {
-		this.resposta = resposta;
-	}
 }
