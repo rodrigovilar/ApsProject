916555a60a05767020c37e9152ecc4e5f8ca8522
jonasmendonca <>
2013-10-07 23:09:30 -0300

    refatorando a classe professor

--- src/Model/Professor.java
+++ src/Model/Professor.java
@@ -1,23 +1,8 @@
 package Model;
 
-import java.io.Serializable;
 
-//@SuppressWarnings("serial")
-public class Professor extends Usuario  implements Serializable { //
-	/*private String nome;
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public int getID() {
-		return ID;
-	}
-	public void setID(int iD) {
-		ID = iD;
-	}
-	private int ID;*/
+public class Professor extends Usuario{ 
+	
 	public Professor(){
 		int id = super.getId();
 		id++;
