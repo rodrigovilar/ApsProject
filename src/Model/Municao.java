c23611b0c2f4bc606c456da10a15b2de658e006c
Josue da Silva Gomes Junior <josue.gomes@dce.ufpb.br>
2013-10-07 22:25:00 -0300

    commitando classe

--- src/Model/Municao.java
+++ src/Model/Municao.java
@@ -3,11 +3,7 @@ package Model;
 import Excecao.BalasEsgotadasException;
 
 public class Municao {
-	private int quantidadeDeBalas = 10;
-
-	public int getQuantidadeDeBalas() {
-		return quantidadeDeBalas;
-	}
+	
 	
 	public void setQuantidadeDeBalas(int quantidadeDeBalas) {
 		this.quantidadeDeBalas = quantidadeDeBalas;
