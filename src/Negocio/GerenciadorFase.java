c692ff4b26ab365b625ee86f0709415ce9d4a28b
jonnathann <>
2013-10-07 22:36:19 -0300

    comitando

--- src/Negocio/GerenciadorFase.java
+++ src/Negocio/GerenciadorFase.java
@@ -7,23 +7,7 @@ import Model.Fase;
 
 public class GerenciadorFase {
 	
-private ArrayList<Fase> fases = new ArrayList<Fase>();
-	
-	public void inserirJogadorNaFase(Jogador jogador, Fase fase) throws FaseNaoDisponivelException{
-		boolean faseLiberada = false;
-		for(Fase f:fases){
-			if(f.isLiberado() && (f.getNivel() == fase.getNivel())){
-				f.criarJogador(jogador);
-				faseLiberada = true;
-				
-			}
-		}
-		if(faseLiberada == false){
-			throw new FaseNaoDisponivelException("Fase ainda não liberada"); 
-		}
-		
-	}
-	
+
 	public boolean getIsFaseLiberada(){
 		if(fases.size() > 0){
 			return true;
