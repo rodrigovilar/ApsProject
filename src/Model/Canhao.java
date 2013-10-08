867bc1953c7bea2379be8f77374e93b9b84dbd8a
Administrador <Administrador@10.10.2.26>
2013-10-07 22:14:10 -0300

    metodo getPosicaoX

--- src/Model/Canhao.java
+++ src/Model/Canhao.java
@@ -7,7 +7,9 @@ public class Canhao {
 	private int posicaoX = 250;//Posi�‹o X do canhao em rela�‹o a tela
 	private int posicaoY = 500;//Posi�‹o Y do canhao em rela�‹o a tela
 	
-	
+	public int getPosicaoX() {//MŽtodo que recupera atributo privado posicaoX
+		return posicaoX;
+	}
 	
 	public void setPosicaoX(int posicaoX) {//MŽtodo que muda o valor do atributo posicaoX
 		this.posicaoX = posicaoX;
