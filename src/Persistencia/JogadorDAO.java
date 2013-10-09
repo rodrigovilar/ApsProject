70425759043034af68d6a0108ac990d610de8999
jonasmendonca <jonas.mendonca@dce.ufpb.br>
2013-10-09 00:01:49 -0300

    comit

--- src/Persistencia/JogadorDAO.java
+++ src/Persistencia/JogadorDAO.java
@@ -12,4 +12,26 @@ import Negocio.Jogador;
 
 public class JogadorDAO {
 
+private static File fileJogador = new File("JogadorPersistência.ser");
 	
+	public void insert(ArrayList<Jogador> jogadores) throws IOException{
+		FileOutputStream fileOut = new FileOutputStream(fileJogador);
+		ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+		objOut.writeObject(jogadores);
+		objOut.close();
+		}
+	
+	@SuppressWarnings("unchecked")
+	public ArrayList<Jogador> selectAll() throws IOException, Exception{
+		ArrayList<Jogador> listaAux;
+		FileInputStream fileIn = new FileInputStream(fileJogador);
+		ObjectInputStream objIn = new ObjectInputStream(fileIn);
+		listaAux = (ArrayList<Jogador>) objIn.readObject();
+		objIn.close();
+		return listaAux;
+	}
+	
+	public static void deletarFileJogador(){
+		fileJogador.delete();
+	}
+}
