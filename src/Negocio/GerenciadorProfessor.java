515939e72ad19b6932d76d2bad011d22b3326802
jonasmendonca <jonas.mendonca@dce.ufpb.br>
2013-10-09 00:24:14 -0300

    comit

--- src/Negocio/GerenciadorProfessor.java
+++ src/Negocio/GerenciadorProfessor.java
@@ -5,8 +5,15 @@ import java.util.ArrayList;
 import Excecao.ObjetoInexistenteException;
 import Excecao.ObjetoJaExistenteException;
 import Model.Professor;
+import Persistencia.ProfessorDAO;
 
 
 public class GerenciadorProfessor {
+	private ArrayList<Professor> professores = new ArrayList<Professor>();
+	private GerenciadorProblema gerenciadorProblema = new GerenciadorProblema();
+	private ProfessorDAO professorDAO = new ProfessorDAO();
 	
+	public boolean loginProfessor(Professor professor) throws Exception{
+		return buscarProfessor(professor);
+	}
 }
