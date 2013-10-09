8c40141f8d16d190bed733d3f86d81af5c339fa0
jonasmendonca <jonas.mendonca@dce.ufpb.br>
2013-10-09 00:13:05 -0300

    comit

--- src/Negocio/GerenciadorJogador.java
+++ src/Negocio/GerenciadorJogador.java
@@ -1,9 +1,17 @@
 package Negocio;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import Excecao.ObjetoInexistenteException;
 import Excecao.ObjetoJaExistenteException;
+import Persistencia.JogadorDAO;
 public class GerenciadorJogador {
 	
+	private ArrayList<Jogador> jogadores = new ArrayList<Jogador>();
+	private JogadorDAO jogadorDAO = new JogadorDAO();
+	
+	public boolean loginJogador(Jogador jogador) throws IOException, Exception{
+		return buscarJogador(jogador);
+	}
 }
