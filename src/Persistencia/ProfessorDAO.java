ac7221dff7104a5e704666e9fa82ee9e9fc4f011
jonasmendonca <jonas.mendonca@dce.ufpb.br>
2013-10-09 00:03:35 -0300

    comit

--- src/Persistencia/ProfessorDAO.java
+++ src/Persistencia/ProfessorDAO.java
@@ -1,8 +1,10 @@
 package Persistencia;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
@@ -18,7 +20,20 @@ private static File fileProfessor ;
 		FileOutputStream fileOut = new FileOutputStream(fileProfessor);
 		ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
 		objOut.writeObject(professores);
-		objOut.close();
-		
+		objOut.close();	
+	}
+	
+	@SuppressWarnings("unchecked")
+	public ArrayList<Professor> selectAll() throws IOException, Exception{
+		ArrayList<Professor> listaAux;
+		FileInputStream fileIn = new FileInputStream(fileProfessor);
+		ObjectInputStream objIn = new ObjectInputStream(fileIn);
+		listaAux = (ArrayList<Professor>) objIn.readObject();
+		objIn.close();
+		return listaAux;
+	}
+	
+	public static void deletarFileProfessor(){
+		fileProfessor.delete();
 	}
 }
