d6fb0b9b600c10800c7bb3a1c9dca4d0215297c7
Josue da Silva Gomes Junior <josue.gomes@dce.ufpb.br>
2013-10-09 00:08:37 -0300

    comit

--- src/Negocio/GerenciadorFase.java
+++ src/Negocio/GerenciadorFase.java
@@ -6,5 +6,21 @@ import Excecao.JogadorNaoLogadoException;
 import Model.Fase;
 
 public class GerenciadorFase {
+
+private ArrayList<Fase> fases = new ArrayList<Fase>();
 	
+	public void inserirJogadorNaFase(Jogador jogador, Fase fase) throws FaseNaoDisponivelException{
+		boolean faseLiberada = false;
+		for(Fase f:fases){
+			if(f.isLiberado() && (f.getNivel() == fase.getNivel())){
+				f.criarJogador(jogador);
+				faseLiberada = true;
+				
+			}
+		}
+		if(faseLiberada == false){
+			throw new FaseNaoDisponivelException("Fase ainda não liberada"); 
+		}
+		
+	}
 }
