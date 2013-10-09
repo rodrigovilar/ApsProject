dd9b2c229070e2e6a383f269db47c58006bdf8a0
jonasmendonca <jonas.mendonca@dce.ufpb.br>
2013-10-09 00:17:17 -0300

    comit

--- src/Negocio/GerenciadorProblema.java
+++ src/Negocio/GerenciadorProblema.java
@@ -11,7 +11,21 @@ import Excecao.ObjetoInexistenteException;
 import Excecao.ObjetoJaExistenteException;
 import Model.Balao;
 import Model.Problema;
+import Persistencia.ProblemaDAO;
 
 public class GerenciadorProblema {
+	private ArrayList<Problema> problemas = new ArrayList<Problema>();
+	private boolean isProfessorLogado;
+	private GerenciadorFase gerenciadorFase = new GerenciadorFase();
+	private ArrayList<Balao> baloes= new ArrayList<Balao>();
+	private ProblemaDAO problemaDAO = new ProblemaDAO();
 	
+	public void cadastrarProblema(Problema problema) throws Exception {
+		if(!isProfessorLogado){
+			throw new LoginInexistenteException("Você não tem permissão para cadastrar o problema!");
+		}
+		problemas.add(problema);
+		problemaDAO.insert(problemas);
+		
+	}
 }
