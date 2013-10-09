5ef942231acd6f2e8dac2bf3db621076d7266431
Josue da Silva Gomes Junior <josue.gomes@dce.ufpb.br>
2013-10-08 23:57:21 -0300

    comit

--- src/Persistencia/ProblemaDAO.java
+++ src/Persistencia/ProblemaDAO.java
@@ -1,5 +1,22 @@
 package Persistencia;
 
-public class ProblemaDAO {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
+import Model.Problema;
+
+public class ProblemaDAO {
+	
+private static File fileProblema  = new File("ProblemaPersistência.ser");;
+	
+	public void insert(ArrayList<Problema> problemas) throws IOException{
+		FileOutputStream fileOut = new FileOutputStream(fileProblema);
+		ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+		objOut.writeObject(problemas);
+		objOut.close();
+		
+	}
 }
