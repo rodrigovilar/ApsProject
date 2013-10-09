8c2dd747243a4ec94d4e82f3a3e41b99c1606368
jonasmendonca <jonas.mendonca@dce.ufpb.br>
2013-10-09 00:23:46 -0300

    alterando

--- src/Negocio/GerenciadorProfessor.java
+++ src/Negocio/GerenciadorProfessor.java
@@ -8,50 +8,5 @@ import Model.Professor;
 
 
 public class GerenciadorProfessor {
-	private ArrayList<Professor> professores = new ArrayList<Professor>();
-	private GerenciadorProblema gerenciadorProblema = new GerenciadorProblema();
 	
-	
-	public boolean loginProfessor(Professor professor){
-		return buscarProfessor(professor);
-	}
-	
-	public GerenciadorProblema getGerenciadorProblema(){
-		return gerenciadorProblema;
-	}
-	
-	public void cadastrarProfessor(Professor professor) throws ObjetoJaExistenteException, ObjetoInexistenteException, IOException {
-		if(buscarProfessor(professor)){
-			throw new ObjetoJaExistenteException("Esse professor já existe");
-		}
-		if(professor.getSenha() == null || professor.getSenha().length() < 4){
-			throw new ObjetoInexistenteException("Senha invï¿½lida");
-		}
-		professores.add(professor);
-		
-	}
-	
-	public void removerProfessor(Professor professor) throws ObjetoInexistenteException{
-		if(!buscarProfessor(professor)){
-			throw new ObjetoInexistenteException("Esse professor nï¿½o existe");
-		}
-		professores.remove(professor);
-	}
-	
-	private boolean buscarProfessor(Professor professor) {
-		for(Professor p:professores){
-			if(p.getNome().equals(professor.getNome())){
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public int getQuantidadeDeProfessoresCadastrados(){
-		return professores.size();
-	}
-	
-	public ArrayList<Professor> listarProfessores(){
-		return professores;
-	}
 }
