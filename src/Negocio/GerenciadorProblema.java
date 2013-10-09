c86d7e5d4b84bb4c2ecb10c300e21dd1e4aad678
jonasmendonca <jonas.mendonca@dce.ufpb.br>
2013-10-09 00:18:28 -0300

    comit

--- src/Negocio/GerenciadorProblema.java
+++ src/Negocio/GerenciadorProblema.java
@@ -32,4 +32,8 @@ public class GerenciadorProblema {
 	public GerenciadorFase getGerenciadorFase(){
 		return gerenciadorFase;
 	}
+	
+	public boolean getIsLogado(){
+		return isProfessorLogado;
+	}
 }
