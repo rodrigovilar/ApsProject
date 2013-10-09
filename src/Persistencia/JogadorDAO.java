9be6e245224099c909889f5600d50654a8006bfe
Josue da Silva Gomes Junior <josue.gomes@dce.ufpb.br>
2013-10-08 23:55:17 -0300

    comitando

--- src/Persistencia/JogadorDAO.java
+++ src/Persistencia/JogadorDAO.java
@@ -1,5 +1,22 @@
 package Persistencia;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import Negocio.Jogador;
+
 public class JogadorDAO {
 
+	private static File fileJogador = new File("JogadorPersistência.ser");
+	
+	public void insert(ArrayList<Jogador> jogadores) throws IOException{
+		FileOutputStream fileOut = new FileOutputStream(fileJogador);
+		ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+		objOut.writeObject(jogadores);
+		objOut.close();
+		
+	}
 }
