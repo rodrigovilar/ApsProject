abbdb580a89aa7446319fae63c996cb69b1fe136
jonasmendonca <jonas.mendonca@dce.ufpb.br>
2013-10-09 00:02:52 -0300

    comit

--- src/Persistencia/ProfessorDAO.java
+++ src/Persistencia/ProfessorDAO.java
@@ -1,5 +1,24 @@
 package Persistencia;
 
-public class ProfessorDAO {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
+import Model.Professor;
+
+public class ProfessorDAO {
+private static File fileProfessor ;
+	
+	public ProfessorDAO(){
+		fileProfessor = new File("ProfessorPersistência.ser");
+	}
+	public void insert(ArrayList<Professor> professores) throws IOException{
+		FileOutputStream fileOut = new FileOutputStream(fileProfessor);
+		ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+		objOut.writeObject(professores);
+		objOut.close();
+		
+	}
 }
